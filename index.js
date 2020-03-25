afe2409df86af7c8984fa0f29e5da536e570b8fb
Mustafa Saber <mostafa.saber3789@gmail.com>
2020-03-25 17:11:51 +0200

    update function code

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-exports.bugsnag = (req, res) => {
-  if (req.body.account.id === process.env.BUGSNAG_ACCOUNT_ID) {
-    const request = require('request');
+exports.handler = async (event) => {
+  // TODO implement
+  let body = JSON.parse(event.body);
+  if (body.account.id === process.env.BUGSNAG_ACCOUNT_ID) {
+    console.log("INSIDE IF");
+    const fetch = require('node-fetch');
     const jbuilder = require('jbuilder');
-    var bugsnagError = req.body.error;
+    var bugsnagError = body.error;
 
-
-
-    var messageBody = jbuilder.encode(function(json) {
-      json.set('cards', function(json) {
-        json.child(function(json) {
-          json.set('header', function(json) {
+    console.log(bugsnagError);
+    var messageBody = jbuilder.encode(function (json) {
+      json.set('cards', function (json) {
+        json.child(function (json) {
+          json.set('header', function (json) {
             json.set('title', `${bugsnagError.exceptionClass} in ${bugsnagError.context}`);
             json.set('subtitle', bugsnagError.message);
           });
 
-          json.set('sections', function(json) {
-            json.child(function(json) {
-              json.set('widgets', function(json) {
-                json.child(function(json) {
-                  json.set('buttons', function(json) {
-                    json.child(function(json) {
-                      json.set('textButton', function(json) {
+          json.set('sections', function (json) {
+            json.child(function (json) {
+              json.set('widgets', function (json) {
+                json.child(function (json) {
+                  json.set('buttons', function (json) {
+                    json.child(function (json) {
+                      json.set('textButton', function (json) {
                         json.set('text', 'VIEW ON BUGSNAG');
-                        json.set('onClick', function(json) {
-                          json.set('openLink', function(json) {
+                        json.set('onClick', function (json) {
+                          json.set('openLink', function (json) {
                             json.set('url', bugsnagError.url);
                           });
                         });
@@ -31,11 +33,11 @@ exports.bugsnag = (req, res) => {
                     });
 
                     if (typeof bugsnagError.createdIssue != "undefined") {
-                      json.child(function(json) {
-                        json.set('textButton', function(json) {
+                      json.child(function (json) {
+                        json.set('textButton', function (json) {
                           json.set('text', `VIEW ON ${bugsnagError.createdIssue.type.toUpperCase()}`);
-                          json.set('onClick', function(json) {
-                            json.set('openLink', function(json) {
+                          json.set('onClick', function (json) {
+                            json.set('openLink', function (json) {
                               json.set('url', bugsnagError.createdIssue.url);
                             });
                           });
@@ -51,22 +53,34 @@ exports.bugsnag = (req, res) => {
       });
     });
 
+    // messageBody = {
+    //   "text": "HI FROM LAMBDA"
+    // };
+
+    console.log("AFTER JSON");
+    console.log(messageBody);
+
     var options = {
-      url: process.env.GOOGLE_CHAT_URL,
+      // url: process.env.GOOGLE_CHAT_URL,
+      method: 'post',
       body: messageBody,
       headers: {
-        'content-type': 'application/json'
-      }
+        'Content-Type': 'application/json'
+      },
     };
 
-    request.post(options, function(error, response, body) {
-      if(error) {
-        res.status(500).send(error);
-      } else {
-        res.status(204).send();
-      }
-    });
+    console.log(options);
+
+
+    return fetch(process.env.GOOGLE_CHAT_URL, options)
+      .then(res => res.json())
+      .then(json => console.log(json));
+
   } else {
-    res.status(402).send('Not authorized');
+    const response = {
+      statusCode: 402,
+      body: {"failed": "Not authorized"},
+    };
+    return response;
   }
 };
